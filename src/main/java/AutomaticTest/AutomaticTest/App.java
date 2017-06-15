1940c92e844d8e3ebb4ea6237f1b64d2fb662e1d
zhangx511 <zhangx511@chinaunicom.cn>
2017-06-15 14:14:29 +0800

    创建项目

---
+++ src/main/java/AutomaticTest/AutomaticTest/App.java
@@ -0,0 +1,13 @@
+package AutomaticTest.AutomaticTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
